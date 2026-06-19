3553b00dc53375637f2468ec988263008196149e
jpdeveloper89-cmd <jpdeveloper89@gmail.com>
2026-06-19 19:45:37 +0900

    build: static export of web app for GitHub Pages

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/drivx/app',
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
     NEXT_PUBLIC_APP_NAME: 'DrivX Protocol',
---
+++ packages/web/out/_next/static/chunks/app/docs/page-a07507a1ae2855ff.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[726,690,860,931],{6957:function(n,e,u){Promise.resolve().then(u.t.bind(u,5469,23))}},function(n){n.O(0,[469,293,286,744],function(){return n(n.s=6957)}),_N_E=n.O()}]);
